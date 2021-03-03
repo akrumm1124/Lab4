0259837bebeda42c2e2e8c58c78e2d372120cae2
Alex <akrumm1124@gmail.com>
2021-03-03 11:34:15 -0600

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter your age, so that your insurance rate can be calculated.");
+    int age = keyboard.nextInt();
+    calculateInsurance(age);
+    }
+    public static void calculateInsurance (int age) {
+        int insurancePrice = (age * 5) + 300;
+        System.out.println("Since you are " + age + " years old, your insurance will cost $" + insurancePrice + " per year.");
+    }
+}
